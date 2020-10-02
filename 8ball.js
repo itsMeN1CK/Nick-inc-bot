f31a5cfb484632eb6ea3c12e17998defe27c4bab
itsMeN1CK <itsMeN1CK@users.noreply.github.com>
2020-10-02 20:29:58 +0530

    Update 8ball.js

--- 8ball.js
+++ 8ball.js
@@ -4,3 +4,15 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+const {MessageEmbeds} = require={
+  name: "8ball",
+  description: "Tells your answer",
+  category: "fun",
+  usage: "<question>",
+  run: async(bot,Message,args)=>{
+ let question= messageC
+
+  }
+
+  
+}
