596b2588e53955e9f79c136cd3c4d6077efc9385
itsMeN1CK <itsMeN1CK@users.noreply.github.com>
2020-10-02 14:57:53 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const token = process.env.token
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
