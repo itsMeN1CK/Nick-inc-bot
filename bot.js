f0a6a63cb09d1748baf1403979fb185c56314cbf
itsMeN1CK <itsMeN1CK@users.noreply.github.com>
2020-10-02 15:04:48 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.env.token
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -16,46 +16,7 @@ client.on('message', msg => {
     color: 3447003,
     author: {
       name: client.user.username,
-      icon_url: client.user.avatarURLclient.on('message', message => {
-        // Ignore messages that aren't from a guild
-        if (!message.guild) return;
-      
-        // If the message content starts with "!kick"
-        if (message.content.startsWith('-kick')) {
-          // Assuming we mention someone in the message, this will return the user
-          // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
-          const user = message.mentions.users.first();
-          // If we have a user mentioned
-          if (user) {
-            
-            const member = message.guild.member(user);
-            
-            if (member) {
-              
-              member
-                .kick('Optional reason that will display in the audit logs')
-                .then(() => {
-                 
-                  message.reply(`Successfully kicked ${user.tag} ahah GG`);
-                })
-                .catch(err => {
-                
-                  message.reply('I was unable to kick the member');
-                
-                  console.error(err);
-                });
-            } else {
-             
-              message.reply("That user isn't in this server! dude so don't kick invalid users ;-;");
-            }
-            
-          } else {
-            message.reply("You didn't mention the user to kick!  fjdjgf learn how to moderate ");
-          }
-        }
-      }),
-      
-      
+      icon_url: client.user.avatarURL
     },
     title: "Commands",
     url: "https://nick-inc.glitch.me",
@@ -101,7 +62,44 @@ client.on('message', msg => {
 }
 });
 
+client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
+
+  // If the message content starts with "!kick"
+  if (message.content.startsWith('-kick')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      
+      const member = message.guild.member(user);
+      
+      if (member) {
+        
+        member
+          .kick('Optional reason that will display in the audit logs')
+          .then(() => {
+           
+            message.reply(`Successfully kicked ${user.tag} ahah GG`);
+          })
+          .catch(err => {
+          
+            message.reply('I was unable to kick the member');
+          
+            console.error(err);
+          });
+      } else {
+       
+        message.reply("That user isn't in this server! dude so don't kick invalid users ;-;");
+      }
+      
+    } else {
+      message.reply("You didn't mention the user to kick!  fjdjgf learn how to moderate ");
+    }
+  }
+});
 
 
-
-client.login('NzYxMjQyNDk2MjgxMDgzOTE1.X3XwUg.i2oxarUVZlqUqTnAtvbaua214qs');
+client.login('process.env.token');
